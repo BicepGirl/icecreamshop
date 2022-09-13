abaf8952840eab6f95eb9ce7bd14c95dcb8545f2
LinnaJansson <92782066+LinnaJansson@users.noreply.github.com>
2022-09-13 14:07:38 +0200

    insert into alla jävla glasser

--- database.js
+++ database.js
@@ -27,18 +27,57 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             }else{
                 //Skapa glassarna
                 let insert ='INSERT INTO icecream (icecreamName, icecreamPrice, icecreamFlavour, icecreamContent, icecreamDescription, icecreamImg) VALUES (?,?,?,?,?,?)'
-                db.run(insert, ["Chonky Chocolate",10,"Chocolate","CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
-                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.","Big scoop of chocolate ice cream","/img/chocolate.png"])
-                db.run(insert, ["Plain Jane",10,"Vanilla","CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
-                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.","Big scoop of vanilla ice cream","/img/vanilla.png"])
-                db.run(insert, ["Caramell Kevin",12,"Vanilla, Caramell","CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
-                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.","Big scoop of vanilla and caramell ice cream","/img/caramell.png"])
-                db.run(insert, ["Caramell Kevin",12,"Vanilla, Caramell","CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
-                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.","Big scoop of vanilla and caramell ice cream","/img/caramell.png"])
-                db.run(insert, ["Caramell Kevin",12,"Vanilla, Caramell","CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
-                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.","Big scoop of vanilla and caramell ice cream","/img/caramell.png"])
-                db.run(insert, ["Caramell Kevin",12,"Vanilla, Caramell","CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
-                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.","Big scoop of vanilla and caramell ice cream","/img/caramell.png"])
+
+                db.run(insert, ["Chocolate Choke",
+                                10,
+                                "Chocolate",
+                                "CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
+                                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.",
+                                "The scoops of various chocolate ice cream scoops in a wafer, chocolate sprinkles, cookie crumble ans mini marshmellows",
+                                "/img/chocolate_choke.jpg"])
+
+                db.run(insert, ["Plain Jane",
+                                10,
+                                "Vanilla",
+                                "CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
+                                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.",
+                                "Thee small scoops of traditionall vanilla ice cream topped with sprinkles",
+                                "/img/plain_jane.jpg"])
+
+                      db.run(insert, ["Strawberry Jerry",
+                                10,
+                                "Vanilla, Strawberry"
+                                "CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
+                                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.",
+                                "Vanilla and strawberry ice cream topped with fresh strawberry and raspberry",
+                                "/img/strawberry_jerry.jpg"])
+
+                      db.run(insert, ["Unicorn Droppings",
+                                10,
+                                "lime, strawberry, blueberry ",
+                                "CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
+                                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.",
+                                "three scoops of mixed sorbet flavors in a wafer",
+                                "/img/unicorn_droppings.jpg"])
+
+                      db.run(insert, ["Rainbow Lowe",
+                                10,
+                                "Raspberry, Mango, Bubblegum",
+                                "CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
+                                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.",
+                                "Three soft milky ice cream scoops topped with rainbow chocolate buttons ",
+                                "/img/rainbow.jpg"])
+
+                      db.run(insert, ["Caramel Cara",
+                                10,
+                                "Caramel, coconut",
+                                "CREAM, LIQUID SUGAR (SUGAR, WATER), SKIM MILK, WATER, SUGAR, WHEAT FLOUR, PEANUTS, COCOA (PROCESSED WITH ALKALI), BROWN SUGAR, EGG YOLKS, COCOA POWDER, PEANUT OIL, SOYBEAN OIL, BUTTER (CREAM, SALT), INVERT CANE SUGAR, SALT, CHOCOLATE LIQUOR, EGGS, TAPIOCA FLOUR, PEANUT FLOUR, PEANUT EXTRACT, VANILLA EXTRACT, EGG WHITES, GUAR GUM, COCOA BUTTER, NATURAL FLAVOR, CARRAGEENAN, SOY LECITHIN, BAKING SODA, MALTED BARLEY FLOUR.\n" +
+                                "Allergy Information: CONTAINS MILK, EGGS, WHEAT, PEANUTS AND SOY.",
+                                "Two large scoops coconut and caramel ice cream in a bowl, topped with coconut flakes and frozen raspberry",
+                                "/img/caramell.jpg"])
+
+
+
             }
         })
     }
